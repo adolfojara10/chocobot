d7c4606203eec99778e0773768fcfbf9bb1d0d15
adolfojara10 <gavilanesadolfo@gmail.com>
2023-12-14 22:32:40 -0500

    f_hard() working

--- face-bluetooth/single_posenet.py
+++ face-bluetooth/single_posenet.py
@@ -60,6 +60,7 @@ def f_easy(frame_received, confidence_threshold=0.1):
                 if left_wrist.y < neck.y or left_wrist.y <= nose.y:
                     print("yessss")
                     step+=1
+                    time.sleep(0.5)
                     is_command_sounded = False
                 
 
@@ -120,6 +121,7 @@ def f_easy(frame_received, confidence_threshold=0.1):
                 
                 if left_wrist.x < neck.x or abs(left_wrist.y - left_elbow.y) < 40:
                     print("yessss")
+                    time.sleep(0.25)
                     step+=1
                     is_command_sounded = False
 
@@ -206,128 +208,205 @@ def f_medium(frame_received, confidence_threshold=0.1):
 
     print(str(step))
 
-    if step == 0:
-        if not is_command_sounded:
-            #reproducir sonido
-            #reproduce_sound.f_movenet()
-            reproduce_sound.f_med_movenet(step+1)
-            print("hola")
-            left_foot = None
-            is_command_sounded = True
-            #left_foot = keypoints[15]
-            #right_foot = keypoints[16]
-        
-        try:
-            if left_foot == None and poses[0].FindKeypoint('left_ankle')>0:
-                left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
-                right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
-
-        except:
-            #print()
-            pass
-        
-
-        try:
-            #print(left_foot[0] - keypoints[15][0], "    ---------------------------      ", right_foot[0] - keypoints[16][0])
-            #print(left_foot[0], " --------- ", keypoints[15][0], "    --------------      ", right_foot[0], " ---------------- ", keypoints[16][0], " ++++++++ ", keypoints[15][2], " ++++ ", keypoints[16][2])
-            if (abs(left_foot.y - poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')].y) > 20 or right_foot[0] - poses[0].Keypoints[poses[0].FindKeypoint('right_ankle').y] > 20) and i_clap<2:
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("yes")
-                left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
-                right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
-                i_clap+=1
-
+    if len(poses) > 0:
+        if step == 0:
+            if not is_command_sounded:
+                #reproducir sonido
+                #reproduce_sound.f_movenet()
+                reproduce_sound.f_med_movenet(step+1)
+                print("hola")
+                left_foot = None
+                is_command_sounded = True
+                #left_foot = keypoints[15]
+                #right_foot = keypoints[16]
             
-            if i_clap == 2:
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("yes22222222222222222222222222")
-                step += 1
-                is_command_sounded = False
-                i_clap = 0
-                left_foot, right_foot = None
-                                
-        except:
-            pass
-            #print("exception")
-            #left_foot = keypoints[15]
-            #right_foot = keypoints[16]
+            try:
+                if left_foot == None and poses[0].FindKeypoint('left_ankle')>0:
+                    left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
+                    right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
 
-    elif step == 1:
-
-        if not is_command_sounded:
-            #reproducir sonido
-            reproduce_sound.f_med_movenet(step+1)
-            is_command_sounded = True
-
-        #if (keypoints[0][2] >= confidence_threshold and keypoints[10][2] >= confidence_threshold):
-        #    print(abs(keypoints[0][1] - keypoints[10][1]), "****************** ", abs(keypoints[0][0] - keypoints[10][0]))
-        left_wrist_idx = poses[0].FindKeypoint('left_wrist')
-
-        if left_wrist_idx > 0:
-            left_wrist = poses[0].Keypoints[left_wrist_idx]
-            neck = poses[0].Keypoints[poses[0].FindKeypoint('neck')]
-            left_shoulder = poses[0].Keypoints[poses[0].FindKeypoint('left_shoulder')]
-            
-            if (int(left_wrist.y - neck.y) < 30 and int(left_wrist.y - neck.y) > -30):
-                print("yessss")
-                step+=1
-                is_command_sounded = False
-
-    elif step == 2:
-        
-        if not is_command_sounded:
-            #reproducir sonido
-            reproduce_sound.f_med_movenet(step+1)
-            is_command_sounded = True
-
-        try:
-            if left_foot == None and poses[0].FindKeypoint('left_ankle')>0:
-                left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
-                right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
-
-        except:
-            #print()
-            pass
-        
-
-        try:
-            #print(left_foot[0] - keypoints[15][0], "    ---------------------------      ", right_foot[0] - keypoints[16][0])
-            #print(left_foot[0], " --------- ", keypoints[15][0], "    --------------      ", right_foot[0], " ---------------- ", keypoints[16][0], " ++++++++ ", keypoints[15][2], " ++++ ", keypoints[16][2])
-            if (abs(left_foot.y - poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')].y) > 35 or right_foot[0] - poses[0].Keypoints[poses[0].FindKeypoint('right_ankle').y] > 35) and i_clap<2:
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("yes")
-                time.sleep(1)
+            except:
+                #print()
                 pass
-                left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
-                right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
-                i_clap+=1
-
             
-            if i_clap == 2:
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("***************************************")
-                print("yes22222222222222222222222222")
-                step += 1
-                is_command_sounded = False
-                i_clap = 0
-                left_foot, right_foot = None
-                                
-        except:
-            pass
-            #print("exception")
-            #left_foot = keypoints[15]
-            #right_foot = keypoints[16]
+
+            try:
+                #print(left_foot[0] - keypoints[15][0], "    ---------------------------      ", right_foot[0] - keypoints[16][0])
+                #print(left_foot[0], " --------- ", keypoints[15][0], "    --------------      ", right_foot[0], " ---------------- ", keypoints[16][0], " ++++++++ ", keypoints[15][2], " ++++ ", keypoints[16][2])
+                if (abs(left_foot.y - poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')].y) > 20 or right_foot[0] - poses[0].Keypoints[poses[0].FindKeypoint('right_ankle').y] > 20) and i_clap<2:
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("yes")
+                    left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
+                    right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
+                    i_clap+=1
+
+                
+                if i_clap == 2:
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("yes22222222222222222222222222")
+                    step += 1
+                    is_command_sounded = False
+                    i_clap = 0
+                    left_foot, right_foot = None
+                                    
+            except:
+                pass
+                #print("exception")
+                #left_foot = keypoints[15]
+                #right_foot = keypoints[16]
+
+        elif step == 1:
+
+            if not is_command_sounded:
+                #reproducir sonido
+                reproduce_sound.f_med_movenet(step+1)
+                is_command_sounded = True
+
+            #if (keypoints[0][2] >= confidence_threshold and keypoints[10][2] >= confidence_threshold):
+            #    print(abs(keypoints[0][1] - keypoints[10][1]), "****************** ", abs(keypoints[0][0] - keypoints[10][0]))
+            left_wrist_idx = poses[0].FindKeypoint('left_wrist')
+
+            if left_wrist_idx > 0:
+                left_wrist = poses[0].Keypoints[left_wrist_idx]
+                neck = poses[0].Keypoints[poses[0].FindKeypoint('neck')]
+                left_shoulder = poses[0].Keypoints[poses[0].FindKeypoint('left_shoulder')]
+                
+                if (int(left_wrist.y - neck.y) < 30 and int(left_wrist.y - neck.y) > -30):
+                    print("yessss")
+                    step+=1
+                    is_command_sounded = False
+
+        elif step == 2:
+            
+            if not is_command_sounded:
+                #reproducir sonido
+                reproduce_sound.f_med_movenet(step+1)
+                is_command_sounded = True
+
+            try:
+                if left_foot == None and poses[0].FindKeypoint('left_ankle')>0:
+                    left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
+                    right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
+
+            except:
+                #print()
+                pass
+            
+
+            try:
+                #print(left_foot[0] - keypoints[15][0], "    ---------------------------      ", right_foot[0] - keypoints[16][0])
+                #print(left_foot[0], " --------- ", keypoints[15][0], "    --------------      ", right_foot[0], " ---------------- ", keypoints[16][0], " ++++++++ ", keypoints[15][2], " ++++ ", keypoints[16][2])
+                if (abs(left_foot.y - poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')].y) > 35 or right_foot[0] - poses[0].Keypoints[poses[0].FindKeypoint('right_ankle').y] > 35) and i_clap<2:
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("yes")
+                    time.sleep(3.5)
+                    #pass
+                    #left_foot = poses[0].Keypoints[poses[0].FindKeypoint('left_ankle')]
+                    #right_foot = poses[0].Keypoints[poses[0].FindKeypoint('right_ankle')]
+                    i_clap+=1
+
+                
+                if i_clap == 2:
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("***************************************")
+                    print("yes22222222222222222222222222")
+                    step += 1
+                    is_command_sounded = False
+                    i_clap = 0
+                    f_reset_vars()
+                    f_send_data("1")
+                                    
+            except:
+                pass
+                #print("exception")
+                #left_foot = keypoints[15]
+                #right_foot = keypoints[16]
+
+
+def f_hard(frame_received, confidence_threshold=0.4):
+    global step, is_command_sounded, right_hand
+
+    poses = f_detect(frame_received)
+
+    print(str(step))
+
+    if len(poses) > 0:
+        if step == 0:
+
+            if not is_command_sounded:
+                #reproducir sonido
+                #reproduce_sound.f_movenet()
+                reproduce_sound.f_dif_movenet(step+1)
+                is_command_sounded = True
+
+            rigth_wrist_idx = poses[0].FindKeypoint('right_wrist')
+            rigth_elbow_idx = poses[0].FindKeypoint('right_elbow')
+
+            if rigth_wrist_idx > 0:
+                rigth_wrist = poses[0].Keypoints[rigth_wrist_idx]
+                rigth_elbow = poses[0].Keypoints[poses[0].FindKeypoint('right_elbow')]
+                
+                
+                if rigth_wrist.y < rigth_elbow.y:
+                    print("yessss")
+                    step+=1
+                    time.sleep(1)
+                    is_command_sounded = False
+
+        elif step == 1:
+            if not is_command_sounded:
+                #reproducir sonido
+                reproduce_sound.f_dif_movenet(step+1)
+                is_command_sounded = True
+
+            rigth_wrist_idx = poses[0].FindKeypoint('right_wrist')
+            left_knee_idx = poses[0].FindKeypoint('left_knee')
+
+            if rigth_wrist_idx > 0 and left_knee_idx>0:
+                rigth_wrist = poses[0].Keypoints[rigth_wrist_idx]
+                left_knee = poses[0].Keypoints[poses[0].FindKeypoint('left_knee')]
+                
+                
+                if abs(rigth_wrist.x - left_knee.x)<35:
+                    print("yessss")
+                    step+=1
+                    time.sleep(0.5)
+                    is_command_sounded = False
+
+        elif step == 2:
+            if not is_command_sounded:
+                #reproducir sonido
+                reproduce_sound.f_dif_movenet(step+1)
+                is_command_sounded = True
+
+            left_wrist_idx = poses[0].FindKeypoint('left_wrist')
+            left_elbow_idx = poses[0].FindKeypoint('left_elbow')
+
+            if left_wrist_idx > 0 and left_elbow_idx:
+                left_wrist = poses[0].Keypoints[left_wrist_idx]
+                left_elbow = poses[0].Keypoints[poses[0].FindKeypoint('left_elbow')]
+                
+                
+                if left_wrist.y < left_elbow.y:
+                    print("yessss")
+                    step+=1
+                    time.sleep(1)
+                    is_command_sounded = False
+                    f_reset_vars()
+                    f_send_data("1")
+
+
 
 
 
@@ -349,7 +428,8 @@ if __name__ == "__main__":
 
 
         #f_easy(img)
-        f_medium(img)
+        #f_medium(img)
+        f_hard(img)
         # perform pose estimation (with overlay)
         """poses = f_detect(img)
 
